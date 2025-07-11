df94f627907ca6155fd4277ea0a55d3265c4a473
Allyson Rodrigues <125301945+Rodrigues4567@users.noreply.github.com>
2025-07-11 04:29:26 -0300

    feat: adiciona as funcionalidades de marcar determinada tarefa como 'completa' e deletar uma tarefa

--- frontend/src/components/Task/index.jsx
+++ frontend/src/components/Task/index.jsx
@@ -1,8 +1,20 @@
+import { FaCheck } from "react-icons/fa6";
+import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
 
-function Task({ task }) {
+function Task({ task, onToggleComplete, onDelete }) {
     return (
-        <div className="flex items-center gap-5 bg-orange-600">
-            {task.title}
+        <div className="flex justify-between items-center p-[5px] pl-[8px] rounded-[7px] bg-orange-600">
+
+            <span className={task.completed ? "line-through text-gray-300" : ""}>
+                {task.title}
+            </span>
+
+            <div className="flex justify-center items-center gap-[10px]">
+                <FaCheck onClick={() => onToggleComplete(task.id, task.completed)} className="text-[2.1rem] p-[8px] rounded-[5px] cursor-pointer bg-yellow-600" />
+                <FaRegEdit className="text-[2.1rem] p-[8px] rounded-[5px] cursor-pointer bg-yellow-600" />
+                <FaRegTrashAlt onClick={() => onDelete(task.id)} className="text-[2.1rem] p-[8px] rounded-[5px] cursor-pointer bg-yellow-600" />
+            </div>
+            
         </div>
     )
 }
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -34,19 +34,41 @@ function Home() {
         }
     }
 
+    async function toggleComplete(id, completed) {
+        try {
+            await axios.put(`http://127.0.0.1:5000/tasks/${id}`, {
+                completed: !completed
+            })
+            fetchTasks()
+        }
+        catch(err) {
+            console.log("Erro ao atualizar tarefa:", err)
+        }
+    }
+
+    async function deleteTask(id) {
+        try {
+            await axios.delete(`http://127.0.0.1:5000/tasks/${id}`)
+            fetchTasks()
+        }
+        catch(err) {
+            console.log("Erro ao atualizar tarefa:", err)
+        }
+    }
+
     useEffect(() => {
         fetchTasks()
     }, [])
 
     return (
         <div>
-            <div className="flex justify-center items-center flex-col bg-slate-400 w-[840px]">
+            <div className="flex flex-col bg-slate-400 w-[840px] py-[1rem] px-[6rem]">
                 <h1 className="font-bold text-[2rem] text-center py-[1.5rem]">To-Do List</h1>
 
-                <div className="mb-[2rem]">
-                    <div className="flex justify-center items-center flex-col">
-                        <input type="text" id="input" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Digite sua tarefa" autoComplete="off" />
-                        <button className="bg-red-500" onClick={addTask}>Adicionar</button>
+                <div className="flex-col mb-[2rem] w-full">
+                    <div className="flex justify-center items-center gap-[11px] mb-[12px]">
+                        <input className="w-full p-[10px] rounded-[8px]" type="text" id="input" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Digite sua tarefa" autoComplete="off" />
+                        <button className="bg-red-500 p-[10px] rounded-[8px]" onClick={addTask}>Adicionar</button>
                     </div>
 
                     <select id="select">
@@ -56,10 +78,10 @@ function Home() {
                     </select>
                 </div>
 
-                <ul>
+                <ul className="flex flex-col gap-[12px]">
                     {tasks.map((t) => (
                         <li key={t.id}>
-                            <Task task={t} />
+                            <Task task={t} onToggleComplete={toggleComplete} onDelete={deleteTask} />
                         </li>
                     ))}
                 </ul>
